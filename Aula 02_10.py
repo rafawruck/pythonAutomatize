5ca9e81eb61ac3e1ca850d00f2e461d2e9a99a89
Rafael Lotar Wruck <rafawruck@gmail.com>
2024-10-02 14:44:15 -0300

    Add files via upload

---
+++ Aula 02_10.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 10:50:44 2024
+
+@author: Rafael Wruck
+GitHub rafawruck
+"""
+print ("Olá mundo")
+print ("Qual o seu nome: ")
+myName = input()
+print ("É bom conhecer você "+ myName)
+print ("O comprimento do seu nome é:")
+print (len(myName))
+print ('Qual é a sua idade?')
+myAge = input()
+print ('Você terá ' + str(int(myAge)+1) + " em um ano")
