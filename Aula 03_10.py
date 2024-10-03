6c47a6281f66253c8f2946685db650961c9cd803
Rafael Lotar Wruck <rafawruck@gmail.com>
2024-10-03 14:53:24 -0300

    Add files via upload

---
+++ Aula 03_10.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 14:32:12 2024
+
+@author: Rafael Wruck
+GitHub rafawruck
+"""
+
+name = input("Informe seu nome: ")
+age = int(input("Informe sua idade: "))
+
+if name == "Alice":
+    print ("Oi, Alice.")
+elif (age < 12):
+    print ("Você não é Alice, garota")
+elif (age < 100):
+    print ("Você não é Alice, vovó")
+elif (age > 2000):
+    print ("Ao contrário de você, Alice não é uma vampira imortal e morta-viva ")
